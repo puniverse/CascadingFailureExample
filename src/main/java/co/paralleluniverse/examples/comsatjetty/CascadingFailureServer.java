0cb2096a8a60369bff468d03955842381cb42adb
eitan <eitan@paralleluniverse.co>
2014-05-25 23:17:02 +0300

    remove clients code

--- src/main/java/co/paralleluniverse/examples/comsatjetty/CascadingFailureServer.java
+++ src/main/java/co/paralleluniverse/examples/comsatjetty/CascadingFailureServer.java
@@ -1,7 +1,5 @@
 package co.paralleluniverse.examples.comsatjetty;
 
-import com.codahale.metrics.MetricRegistry;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -10,14 +8,11 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class CascadingFailureServer {
     private static final int THREAD_COUNT_DEFAULT = 200;
-     static final int TIMEOUT = 30000;
+    static final int TIMEOUT = 30000;
     static final int MAX_CONN = 99999;
-    static final MetricRegistry metrics = new MetricRegistry();
-    static AtomicInteger ai = new AtomicInteger();
-    final static String SERVICE_URL = "http://localhost:8080/target?sleep=";
+    static final String SERVICE_URL = "http://localhost:8080/target?sleep=";
 
     public static void main(String[] args) throws Exception {
-//        ConsoleReporter.forRegistry(metrics).build().start(2, TimeUnit.SECONDS);
         int threads = args.length > 0 ? Integer.parseInt(args[0]) : THREAD_COUNT_DEFAULT;
         System.out.println("Serving using " + threads + " threads....");
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -27,50 +22,13 @@ public class CascadingFailureServer {
         context.addServlet(new ServletHolder(TagetServlet.class), "/target");
 
         server.start();
-        System.out.println("http://localhost:8080/regular");
-        System.out.println("http://localhost:8080/fiber");
-        System.out.println("http://localhost:8080/simple");
-        System.out.println("http://localhost:8080/shutdown");
+        System.out.println("http://localhost:8080/regular?sleep=5000&callService=true");
+        System.out.println("http://localhost:8080/fiber?sleep=5000&callService=true");
         server.join();
     }
 
     private static Server createJettyServer(int threads, ServletContextHandler context) {
-//        final Timer execute = metrics.timer("execute");
-//        final Timer jobrun = metrics.timer("jobrun");
-
-        // Use this in order to measure delay times in the queue
-        final QueuedThreadPool queuedThreadPool = new QueuedThreadPool(threads,threads) {
-//            AtomicInteger ai2 = new AtomicInteger();
-//
-//            @Override
-//            public void execute(Runnable job) {
-//                Timer.Context executeCtx = execute.time();
-//                super.execute(() -> {
-//                    Timer.Context runCtx = jobrun.time();
-//                    job.run();
-//                    try {
-//                        if (job.getClass().getName().equals("org.eclipse.jetty.io.AbstractConnection$2")) {
-//                            Field f = job.getClass().getDeclaredField("this$0");
-//                            if (f != null && f.getType().isAssignableFrom(HttpConnection.class)) {
-//                                f.setAccessible(true);
-//                                HttpConnection conn = (HttpConnection) f.get(job);
-//                                String pathInfo = conn.getHttpChannel().getRequest().getPathInfo();
-//                                if ("/target".equals(pathInfo)) {
-//                                    executeCtx.stop();
-//                                    runCtx.stop();
-//                                }
-//                            }
-//                        }
-//                    } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
-//                        Logger.getLogger(CascadingFailureServer.class.getName()).log(Level.SEVERE, null, ex);
-//                    }
-//                });
-//            }
-        };
-
-//        metrics.register("waiting jobs", (Gauge<Integer>) () -> queuedThreadPool.getQueueSize());
-//        metrics.register("threads num", (Gauge<Integer>) () -> queuedThreadPool.getThreads());
-        final Server server = new Server(queuedThreadPool);
+        final Server server = new Server(new QueuedThreadPool(threads, threads));
         ServerConnector http = new ServerConnector(server);
         http.setPort(8080);
         http.setAcceptQueueSize(MAX_CONN);
@@ -78,5 +36,4 @@ public class CascadingFailureServer {
         server.setHandler(context);
         return server;
     }
-
 }
