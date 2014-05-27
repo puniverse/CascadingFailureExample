ae9c73e063e59c3d6c9a47c56c9632c07beb25dd
eitan <eitan@paralleluniverse.co>
2014-05-27 16:42:14 +0300

    -

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,12 +10,12 @@ loadrate=$4
 # Warmup
 for rate in 100 500 1000 $loadrate
 do
-    java -jar photon.jar  -name Warmup -rate $rate http://${hostname}:8080/${path}\?sleep=50\&callService\=true -duration 5 -print 0
+    java -jar photon.jar  -name Warmup${rate} -rate $rate http://${hostname}:8080/${path}\?sleep=50\&callService\=true -duration 5 -print 0
 done
 
 # Tested service
-java -jar photon.jar  -name SmallService -rate 10 http://${hostname}:8080/${path} -duration 60 -stats & 
+java -jar photon.jar -name ${path}FastService -rate 10 http://${hostname}:8080/${path} -duration 60 -stats -print 5000 & 
 
 # Load service
-java -jar photon.jar  -name Load -rate $loadrate http://${hostname}:8080/${path}\?sleep=1\&callService\=true -duration 5 -stats
-java -jar photon.jar  -name Load -rate $loadrate http://${hostname}:8080/${path}\?sleep=${sleeptime}\&callService\=true -duration 50 -stats
+java -jar photon.jar -name ${path}Load1 -rate $loadrate http://${hostname}:8080/${path}\?sleep=1\&callService\=true -duration 5 -stats -print 5000
+java -jar photon.jar -name ${path}Load2 -rate $loadrate http://${hostname}:8080/${path}\?sleep=${sleeptime}\&callService\=true -duration 50 -stats -print 5000
