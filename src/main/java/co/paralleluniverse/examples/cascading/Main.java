27fbf8b282388e272b4eb05b3b244c1bfc9cbdbd
pron <ron.pressler@gmail.com>
2014-05-27 23:58:08 +0300

    -

--- src/main/java/co/paralleluniverse/examples/cascading/Main.java
+++ src/main/java/co/paralleluniverse/examples/cascading/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static final int TIMEOUT = 30000;
     public static final int MAX_CONN = 120000;
     public static final int PORT = 8080;
-    //
+    public static final String SERVICE_URL = "http://localhost:8080/internal/foo?sleep=";
+    
     private static final String PARAM_JERSEY_PACKAGES = "jersey.config.server.provider.packages";
     private static final Class JERSEY_SERVLET = org.glassfish.jersey.servlet.ServletContainer.class;
     private static final Class JERSEY_FIBER_SERVLET = co.paralleluniverse.fibers.jersey.ServletContainer.class;
     private static final String PACKAGE_NAME_PREFIX = Main.class.getPackage().getName() + ".";
 
-    public static final String SERVICE_URL = "http://localhost:8080/internal/foo?sleep=";
-
     public static void main(String[] args) throws Exception {
-        Options options = new Options();
+        final Options options = new Options();
         options.addOption("threads", true, "number of threads");
         options.addOption("server", true, "jetty/tomcat/undertow");
         options.addOption("fibers", false, "use fibers");
         options.addOption("help", false, "print help");
 
         final CommandLine cmd = new BasicParser().parse(options, args);
+        
         if (cmd.hasOption("help")) {
             new HelpFormatter().printHelp("run.sh server [options]", options);
             return;
@@ -35,7 +35,7 @@ public class Main {
         System.out.println("Serving with " + serverType + " with " + threads + " IO threads - " + (useFibers ? "USING" : "NOT USING") + " FIBERS");
         System.out.println("http://localhost:8080/api/service?sleep=5000");
 
-        EmbeddedServer server;
+        final EmbeddedServer server;
         switch (serverType) {
             case "jetty":
                 server = new JettyServer();
@@ -55,7 +55,7 @@ public class Main {
         server.setPort(PORT).setMaxConnections(MAX_CONN).setNumThreads(threads);
         server.addServlet("internal", JERSEY_FIBER_SERVLET, "/internal/*")
                 .setInitParameter(PARAM_JERSEY_PACKAGES, PACKAGE_NAME_PREFIX + "internal");
-        server.addServlet("service", useFibers ? JERSEY_FIBER_SERVLET : JERSEY_SERVLET, "/api/*")
+        server.addServlet("service", useFibers ? JERSEY_FIBER_SERVLET : JERSEY_SERVLET, "/api/*") // <-------------
                 .setInitParameter(PARAM_JERSEY_PACKAGES, PACKAGE_NAME_PREFIX + (useFibers ? "fibers" : "plain"));
 
         server.run();
