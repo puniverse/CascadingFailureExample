f2eaa4e6eb4aa9d86b27869bf3b96b426016625c
pron <ron.pressler@gmail.com>
2014-05-28 00:10:05 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$#" == "0" ]; then
-	echo "Usage: $0 task arg0 args1 ... argN"
+	echo "Usage: $0 arg0 args1 ... argN"
 	exit 1
 fi
 taskname=$1
 shift
 if [ "$#" == "0" ]; then
-	./gradlew -q $taskname 
+	./gradlew run
 else
-	./gradlew -q $taskname -Pargs="$*" 
+	./gradlew run -Pargs="$*" 
 fi
