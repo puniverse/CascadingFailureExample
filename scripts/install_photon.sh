e1e518a96e5f58166abe22066314d3e78075f5c7
eitan <eitan@paralleluniverse.co>
2014-05-27 15:01:08 +0300

    change scripts

--- scripts/install_photon.sh
+++ scripts/install_photon.sh
@@ -1,2 +1,7 @@
-wget https://github.com/puniverse/photon/releases/download/v0.2.0/photon.jar
+#wget https://github.com/puniverse/photon/releases/download/v0.2.0/photon.jar
+git clone https://github.com/puniverse/photon.git
+cd photon
+./gradlew fullCapsule
+cp build/libs/photon.jar ..
+cd ..
 java -jar photon.jar -help
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,27 +1,21 @@
-hostname="localhost"
-path="regular"
-
-if [ $# -lt 2 ]; then
-    echo "Your command line contains $# arguments"
-    echo usage: $0 hostname regular/fiber
+if [ $# -lt 4 ]; then
+    echo usage: $0 hostname regular/fiber sleep loadrate
     exit
 fi
 hostname=$1
 path=$2
+sleeptime=$3
+loadrate=$4
 
 # Warmup
-for rate in 10 20 100
+for rate in 100 500 1000 $loadrate
 do
-    java -jar photon.jar  -name Warmup -rate $rate http://${hostname}:8080/${path}\?sleep=500\&callService\=true -duration 5
+    java -jar photon.jar  -name Warmup -rate $rate http://${hostname}:8080/${path}\?sleep=50\&callService\=true -duration 5 -print 0
 done
 
 # Tested service
-java -jar photon.jar  -name SmallService -rate 10 http://${hostname}:8080/${path}\?sleep=500\&callService\=false -duration 30 &
+java -jar photon.jar  -name SmallService -rate 10 http://${hostname}:8080/${path} -duration 60 -stats & 
 
 # Load service
-for rate in 100 200 300 400
-do
-    java -jar photon.jar  -name Load -rate $rate http://${hostname}:8080/${path}\?sleep=500\&callService\=true -duration 5
-done
-
-
+java -jar photon.jar  -name Load -rate $loadrate http://${hostname}:8080/${path}\?sleep=1\&callService\=true -duration 5 -stats
+java -jar photon.jar  -name Load -rate $loadrate http://${hostname}:8080/${path}\?sleep=${sleeptime}\&callService\=true -duration 50 -stats
